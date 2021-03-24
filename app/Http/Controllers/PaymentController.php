4d878356448bad267d477be54501b2c1c1aaf644
vagrant <vagrant@homestead>
2021-03-24 05:39:02 +0000

    分期付款

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\OrderPaid;
 use App\Exceptions\InvalidRequestException;
+use App\Models\Installment;
 use App\Models\Order;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
